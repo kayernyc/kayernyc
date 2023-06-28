0fc92cf72d33fc1a714cbf4f3e14258303e77499
Kathrin <kathrin.ayer@gmail.com>
2023-06-28 14:52:12 -0400

    add graphql query
    Added simple GraphQL query to ensure that authorization works, is not revealed and returns some data about the target repository.

--- update-readme.js
+++ update-readme.js
@@ -2,8 +2,35 @@ require('dotenv').config();
 
 const { Octokit } = require("@octokit/rest");
 
-console.log(process.env.OCTOKEY);
+const fetchAllChanges = async () => { 
+  const octokit = new Octokit({
+    userAgent: 'kayernyc v1.2.3',
+    auth: process.env.OCTOKEY
+  });
+  
+  const result =  await octokit.graphql(`
+  query { 
+    repository(name: "til", owner: "kayernyc") {
+      id
+      pushedAt
+      pullRequests(first: 5) {
+        edges {
+          node {
+            id
+            files(first: 5) {
+              nodes {
+                path
+              }
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  }
+  `)
+  
+  console.log(JSON.stringify(result, null, 2))
+}
 
-// const octokit = new Octokit({
-//   userAgent: 'myApp v1.2.3',
-// });
+fetchAllChanges();
